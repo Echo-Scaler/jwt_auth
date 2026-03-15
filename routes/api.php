2e63ac5ddca60a3263e022ff15b28da6582fa261
Wai Yan <waiyan.msc.yu12@gmail.com>
2026-03-16 02:53:23 +0900

    rate_limiting_create

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Middleware\UserMiddleware;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::post('login',[AuthController::class,'login']);
+Route::post('login',[AuthController::class,'login'])->middleware('throttle:5,1'); //rate limiting
 
 // Route::middleware('auth:api')->group(function(){
 //     Route::post('logout',[AuthController::class,'logout']);
